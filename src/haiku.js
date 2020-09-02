05c91f04d0ecdb43cba5145173e8946d7f2b33a7
diegosalazar343 <diegosalazar343@gmail.com>
2020-09-02 11:32:25 -0700

    add business, user, and css

--- src/haiku.js
+++ src/haiku.js
@@ -1,1 +1,10 @@
 //Business Logic
+Function Haiku(line1, line2, line3) {
+  this.line1 = line1;
+  this.line2 = line2;
+  this.line3 = line3;
+}
+
+Haiku.prototype.checkType = Function(){
+  return "Can't anser that yet";
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 // Interface Logic
+$(document).ready(funciton() {
+  $('#haiku-checker-form').submit(function(event) {
+    event.preventDefault();
+    let line1 = $('#line1').val();
+    let line2 = $('#line2').val();
+    let line3 = $('#line3').val();
+    const haiku = new haiku(line1, line2, line3)
+    const response = haiku.checkType();
+    $('#response').append("<p>" + response + "</p>")
+  });
+});
